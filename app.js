baad4889a21e3625704f5167118724543c726a73
Prateek Singh <71702727+prat33k-device@users.noreply.github.com>
2022-09-05 15:47:42 +0530

    project complete

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 // jshint esversion:6
-
+require('dotenv').config()
 const express = require("express");
 const https = require("https");
 const bodyParser = require("body-parser");
@@ -19,7 +19,7 @@ app.post("/", function(req, res) {           // first parameter will be same as 
 
     const city = req.body.cityName;
     const unit = "metric";
-    const appid = "6f52ba990d4dca3c9a161ff78c8ca330"
+    const appid = process.env.API_KEY;
 
     const url = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + appid + "&units=" + unit;
     https.get(url, function(dataFromAPIres){
@@ -28,16 +28,20 @@ app.post("/", function(req, res) {           // first parameter will be same as 
 
         dataFromAPIres.on("data", function(data) {
             const weatherData = JSON.parse(data);         // JSON to object and store it in weatherData
-
-            const temp = weatherData.main.temp;
-            const dis = weatherData.weather[0].description;
-            const icon = weatherData.weather[0].icon;
-            const iconUrl = "http://openweathermap.org/img/wn/" + icon + "@2x.png";
-            res.write("<p>Weather Status : " + dis + "</p>");
-            res.write("<h1>Temperature in " + city + " is " + temp + " celsius</h1>");
-            res.write("<image src=" + iconUrl + ">");
-            res.send();
-            // console.log(weatherData);
+            if(weatherData.cod === 200) {
+                const temp = weatherData.main.temp;
+                const dis = weatherData.weather[0].description;
+                const icon = weatherData.weather[0].icon;
+                const iconUrl = "http://openweathermap.org/img/wn/" + icon + "@2x.png";
+                res.write("<p>Weather Status : " + dis + "</p>");
+                res.write("<h1>Temperature in " + city + " is " + temp + " celsius</h1>");
+                res.write("<image src=" + iconUrl + ">");
+                res.send();
+            }else {
+                res.send("<h1>City Not Found..!</h1>")
+            }
+            
+            console.log(weatherData);
         });
         
     });
